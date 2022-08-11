79389ea03aa206743d6a31d2de067ca38cb50c47
ry-dgel <rydgel.code@gmail.com>
2022-08-11 17:33:18 -0400

    Initial code for picoharp control

--- dpg_confocal_cryo.py
+++ dpg_confocal_cryo.py
@@ -48,9 +48,9 @@ dpg = rdpg.dpg
 log.basicConfig(format='%(levelname)s:%(message)s ', level=log.INFO)
 
 # Setup real control
-log.warning("Using Real Controls")
-fpga = CryoFPGA()
-obj = Objective()
+log.warning("Using Dummy Controls")
+fpga = fpga_cryo_dummy.DummyCryoFPGA()
+obj = objective_dummy.DummyObjective()
 
 # Setup counts data
 counts_data = {'counts':[0],
